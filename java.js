6851c85539d2ea60e950b60b634081f1c8b5ac9c
yuvilesnar <103726563+yuvilesnar@users.noreply.github.com>
2022-05-25 20:23:54 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+
+ 
+ 
+function addition(a,b){
+    var a = Number(prompt("enter a first number"));
+    var b = Number(prompt("enter second number"));
+    
+    document.getElementById("result").innerHTML=a+b+" is the result";
+}
+function subtraction(a,b){
+    var a = Number(prompt("enter a first number"));
+    var b = Number(prompt("enter second number"));
+    document.getElementById("result").innerHTML=a-b+" is the result";
+
+}
+function division(a,b){
+    var a = Number(prompt("enter a first number"));
+    var b = Number(prompt("enter second number"));
+    document.getElementById("result").innerHTML=a/b+" is the result";
+
+}
+
+function multiplication(a,b){
+    var a = Number(prompt("enter a first number"));
+    var b = Number(prompt("enter second number"));
+    document.getElementById("result").innerHTML= a*b+" is the result";
+
+};
+
